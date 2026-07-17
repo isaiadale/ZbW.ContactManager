27af6a7bb9676bc169d0ef6408dd4dbede09be89
Nia <estefania.schmid@outlook.com>
2026-07-17 13:06:42 +0200

    Add: Startseite mit Titel und Kacheln für Kundschaft und Mitarbeitende

--- src/ContactManager/ContactManager.UI.WinForms/MainForm.Designer.cs
+++ src/ContactManager/ContactManager.UI.WinForms/MainForm.Designer.cs
@@ -28,18 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
+            LblHomeTitle = new Label();
+            PnlCustomerTile = new Panel();
+            LblCustomerTile = new Label();
+            PnlEmployeeTile = new Panel();
+            LblEmployeeTile = new Label();
+            PnlCustomerTile.SuspendLayout();
+            PnlEmployeeTile.SuspendLayout();
             SuspendLayout();
+            // 
+            // LblHomeTitle
+            // 
+            LblHomeTitle.Dock = DockStyle.Top;
+            LblHomeTitle.Font = new Font("Century Gothic", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            LblHomeTitle.Location = new Point(0, 0);
+            LblHomeTitle.Name = "LblHomeTitle";
+            LblHomeTitle.Size = new Size(688, 70);
+            LblHomeTitle.TabIndex = 0;
+            LblHomeTitle.Text = "Kontaktübersicht";
+            LblHomeTitle.TextAlign = ContentAlignment.BottomCenter;
+            // 
+            // PnlCustomerTile
+            // 
+            PnlCustomerTile.Controls.Add(LblCustomerTile);
+            PnlCustomerTile.Location = new Point(135, 150);
+            PnlCustomerTile.Name = "PnlCustomerTile";
+            PnlCustomerTile.Size = new Size(200, 150);
+            PnlCustomerTile.TabIndex = 1;
+            // 
+            // LblCustomerTile
+            // 
+            LblCustomerTile.Dock = DockStyle.Fill;
+            LblCustomerTile.Location = new Point(0, 0);
+            LblCustomerTile.Name = "LblCustomerTile";
+            LblCustomerTile.Size = new Size(200, 150);
+            LblCustomerTile.TabIndex = 0;
+            LblCustomerTile.Text = "Kundschaft";
+            LblCustomerTile.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // PnlEmployeeTile
+            // 
+            PnlEmployeeTile.Controls.Add(LblEmployeeTile);
+            PnlEmployeeTile.Location = new Point(375, 150);
+            PnlEmployeeTile.Name = "PnlEmployeeTile";
+            PnlEmployeeTile.Size = new Size(200, 150);
+            PnlEmployeeTile.TabIndex = 2;
+            // 
+            // LblEmployeeTile
+            // 
+            LblEmployeeTile.Dock = DockStyle.Fill;
+            LblEmployeeTile.Location = new Point(0, 0);
+            LblEmployeeTile.Name = "LblEmployeeTile";
+            LblEmployeeTile.Size = new Size(200, 150);
+            LblEmployeeTile.TabIndex = 0;
+            LblEmployeeTile.Text = "Mitarbeitende";
+            LblEmployeeTile.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(11F, 21F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(880, 378);
+            ClientSize = new Size(688, 414);
+            Controls.Add(PnlEmployeeTile);
+            Controls.Add(PnlCustomerTile);
+            Controls.Add(LblHomeTitle);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MinimizeBox = false;
             Name = "MainForm";
-            Text = "ContactManager";
+            Text = "ContactManager - Home";
+            PnlCustomerTile.ResumeLayout(false);
+            PnlEmployeeTile.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Label LblHomeTitle;
+        private Panel PnlCustomerTile;
+        private Label LblCustomerTile;
+        private Panel PnlEmployeeTile;
+        private Label LblEmployeeTile;
     }
 }
--- src/ContactManager/ContactManager.UI.WinForms/MainForm.cs
+++ src/ContactManager/ContactManager.UI.WinForms/MainForm.cs
@@ -1,11 +1,24 @@
+using ContactManager.UI.WinForms.Base;
+
 namespace ContactManager.UI.WinForms
 
 {
-    public partial class MainForm : Base.BaseForm
+    public partial class MainForm : BaseForm
     {
         public MainForm()
         {
             InitializeComponent();
+            // Kacheln gemäss der selbst erstellten Farbpalette
+            PnlCustomerTile.BackColor = AppColors.Primary;
+            PnlEmployeeTile.BackColor = AppColors.Primary;
+
+            // Textfarbe anpassen, da der Background dunkel ist
+            LblCustomerTile.ForeColor = AppColors.TextOnPrimary;
+            LblEmployeeTile.ForeColor = AppColors.TextOnPrimary;
+
+            // Cursor bei Hover auf Hand setzen, zeigt Klickbarkeit der Kacheln an
+            PnlCustomerTile.Cursor = Cursors.Hand;
+            PnlEmployeeTile.Cursor = Cursors.Hand;
         }
     }
 }
