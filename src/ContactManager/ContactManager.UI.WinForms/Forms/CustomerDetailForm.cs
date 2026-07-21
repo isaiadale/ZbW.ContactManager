d9627eae14aae9a350dcc5a430f68bcadcbf1091
Nia <estefania.schmid@outlook.com>
2026-07-21 15:08:30 +0200

    Add: Leere Grundgerüste für CustomerDetailForm und EmployeeDetailForm

---
+++ src/ContactManager/ContactManager.UI.WinForms/Forms/CustomerDetailForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using ContactManager.UI.WinForms.Base;
+
+namespace ContactManager.UI.WinForms.Forms
+{
+    /// <summary>
+    /// Zeigt die Detaildaten einer einzelnen Kundin an bzw. dient der Erfassung
+    /// neuer Kontakte. Wird von <see cref="CustomerListForm"/> aus geöffnet
+    /// (Button "Neuer Kontakt" oder Doppelklick auf einen bestehenden Eintrag).
+    /// </summary>
+    public partial class CustomerDetailForm : BaseForm
+    {
+        public CustomerDetailForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/ContactManager/ContactManager.UI.WinForms/Forms/EmployeeDetailForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using ContactManager.UI.WinForms.Base;
+
+namespace ContactManager.UI.WinForms.Forms
+{
+    /// <summary>
+    /// Zeigt die Detaildaten einer einzelnen Mitarbeitenden-Person an bzw. dient der
+    /// Erfassung neuer Mitarbeitende. Wird von <see cref="EmployeeListForm"/> aus
+    /// geöffnet (Button "Neue Mitarbeitende" oder Doppelklick auf einen bestehenden Eintrag).
+    /// </summary>
+    public partial class EmployeeDetailForm : BaseForm
+    {
+        public EmployeeDetailForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
